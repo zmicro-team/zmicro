348035bda187b0ceaba43241cae3e7a625e2b200
thinkgo <thinkgo@aliyun.com>
2022-12-29 10:28:02 +0800

    fix: skip request response body with rule, add logging recover

--- core/transport/http/middleware/logging/logging.go
+++ core/transport/http/middleware/logging/logging.go
@@ -4,8 +4,12 @@ import (
 	"bytes"
 	"context"
 	"io"
-	"io/ioutil"
+	"mime"
+	"net"
 	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
 	"strings"
 	"time"
 
@@ -49,29 +53,47 @@ func copyHeader(header http.Header) http.Header {
 	for k, v := range header {
 		h[k] = v
 	}
-
 	return h
+}
+
+func skipRequestBody(c *gin.Context) bool {
+	// skip request body rule
+	// skip MultipartForm file
+	v := c.Request.Header.Get("Content-Type")
+	d, params, err := mime.ParseMediaType(v)
+	if err != nil || !(d == "multipart/form-data" || d == "multipart/mixed") {
+		return false
+	}
+	_, ok := params["boundary"]
+	return ok
+}
+
+func skipResponseBody(c *gin.Context) bool {
+	// TODO: add skip response body rule
+	return false
 }
 
 func Log() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var start = time.Now()
-		var writer *rspWriter
-		var buf bytes.Buffer
-		var body string
-		c.Request.Body = ioutil.NopCloser(io.TeeReader(c.Request.Body, &buf))
-		if _, err := ioutil.ReadAll(c.Request.Body); err != nil {
-			c.Abort()
-			return
-		}
 
-		body = buf.String()
-		c.Request.Body = ioutil.NopCloser(&buf)
-		writer = &rspWriter{c.Writer, &bytes.Buffer{}}
-		c.Writer = writer
+		respBodyBuf := &bytes.Buffer{}
+		reqBody := "skip request body"
+
+		if !skipRequestBody(c) {
+			buf := &bytes.Buffer{}
+			c.Request.Body = io.NopCloser(io.TeeReader(c.Request.Body, buf))
+			if _, err := io.ReadAll(c.Request.Body); err != nil {
+				c.Abort()
+				return
+			}
+
+			reqBody = buf.String()
+			c.Request.Body = io.NopCloser(buf)
+		}
+		c.Writer = &rspWriter{c.Writer, respBodyBuf}
 
 		defer func() {
-			// if c.Request.RequestURI
 			if checkPrefix(c.Request.RequestURI, "/swagger") {
 				return
 			}
@@ -92,19 +114,17 @@ func Log() gin.HandlerFunc {
 			fields = append(fields, zap.Duration("duration", duration))
 			fields = append(fields, zap.Any("req", map[string]interface{}{
 				"header": copyHeader(c.Request.Header),
-				"body": func() interface{} {
-					if c.Request.MultipartForm != nil {
-						return "MultipartForm File"
-					} else {
-						return body
-					}
-				}(),
+				"body":   reqBody,
 			}))
 
 			if env.Get() == env.Develop {
+				respBody := "skip response body"
+				if !skipResponseBody(c) {
+					respBody = respBodyBuf.String()
+				}
 				fields = append(fields, zap.Any("rsp", map[string]interface{}{
 					"header": copyHeader(c.Writer.Header()),
-					"body":   writer.body.String(),
+					"body":   respBody,
 				}))
 			}
 			// slow log
@@ -132,4 +152,53 @@ func checkPrefix(s string, prefixes ...string) bool {
 		}
 	}
 	return false
+}
+
+// Recovery returns a gin.HandlerFunc (middleware)
+// that recovers from any panics and logs requests using uber-go/zap.
+// All errors are logged using zap.Error().
+// stack means whether output the stack info.
+// The stack info is easy to find where the error occurs but the stack info is too large.
+func Recovery(stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") ||
+							strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					log.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.ByteString("request", httpRequest),
+					)
+					// If the connection is dead, we can't write a status to it.
+					_ = c.Error(err.(error))
+					c.Abort()
+					return
+				}
+
+				fields := make([]zap.Field, 0, 3)
+				fields = append(fields,
+					zap.Any("error", err),
+					zap.ByteString("request", httpRequest),
+				)
+				if stack {
+					fields = append(fields, zap.ByteString("stack", debug.Stack()))
+				}
+				log.Error("recovery from panic", fields...)
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
+	}
 }
