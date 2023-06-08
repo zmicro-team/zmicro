ff4d70cb4890471b5434bb1a01b2da5938f7d431
gogogo <gogogo@aliyun.com>
2023-06-08 09:52:29 +0800

    fix: drop gin Implemented

--- core/transport/http/helper.go
+++ core/transport/http/helper.go
@@ -1,14 +1,10 @@
 package http
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"google.golang.org/protobuf/encoding/protojson"
 
-	"github.com/zmicro-team/zmicro/core/encoding"
-	"github.com/zmicro-team/zmicro/core/encoding/jsonpb"
 	"github.com/zmicro-team/zmicro/core/errors"
 )
 
@@ -29,88 +25,4 @@ func Error(c *gin.Context, err error) {
 
 func JSON(c *gin.Context, data any) {
 	c.JSON(http.StatusOK, data)
-}
-
-// Deprecated: use Carrier interface
-type Implemented struct {
-	Encoding *encoding.Encoding
-}
-
-// Deprecated: use Carrier interface
-func NewDefaultImplemented() *Implemented {
-	e := encoding.New()
-	err := e.Register(encoding.MIMEJSON, &Codec{
-		Codec: &jsonpb.Codec{
-			MarshalOptions: protojson.MarshalOptions{
-				UseProtoNames:  true,
-				UseEnumNumbers: true,
-			},
-			UnmarshalOptions: protojson.UnmarshalOptions{
-				DiscardUnknown: true,
-			},
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-	return &Implemented{
-		Encoding: e,
-	}
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) Validate(ctx context.Context, v any) error {
-	return Validate(ctx, v)
-}
-
-// Deprecated: use Carrier interface
-func (*Implemented) ValidateMap(ctx context.Context, data map[string]interface{}, rules map[string]interface{}) map[string]interface{} {
-	return Validator().ValidateMapCtx(ctx, data, rules)
-}
-
-// Deprecated: use Carrier interface
-func (*Implemented) ErrorEncoder(c *gin.Context, err error, isBadRequest bool) {
-	ErrorEncoder(c, err, isBadRequest)
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) Bind(c *gin.Context, v any) error {
-	if i.Encoding == nil {
-		return c.ShouldBind(v)
-	}
-	return i.Encoding.Bind(c.Request, v)
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) BindQuery(c *gin.Context, v any) error {
-	if i.Encoding == nil {
-		return c.ShouldBindQuery(v)
-	}
-	return i.Encoding.BindQuery(c.Request, v)
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) BindUri(c *gin.Context, v any) error {
-	if i.Encoding == nil {
-		return c.ShouldBindUri(v)
-	}
-	return i.Encoding.BindUri(c.Request, v)
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) RequestWithUri(req *http.Request, params gin.Params) *http.Request {
-	return RequestWithUri(req, params)
-}
-
-// Deprecated: use Carrier interface
-func (i *Implemented) Render(c *gin.Context, v any) {
-	if i.Encoding == nil {
-		c.JSON(http.StatusOK, v)
-		return
-	}
-	c.Writer.WriteHeader(http.StatusOK)
-	err := i.Encoding.Render(c.Writer, c.Request, v)
-	if err != nil {
-		c.String(http.StatusInternalServerError, "Render failed cause by %v", err)
-	}
 }
